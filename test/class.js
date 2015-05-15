7f6e17bb804c0c0fd5be5fbc1c23ecd3e3afb0ab
Eloy Espinaco <eloyesp@gmail.com>
2015-05-15 12:03:25 -0300

    Add TimeZone (to work in browsers).

--- test/class.js
+++ test/class.js
@@ -1,21 +1,21 @@
 var test = require('tape');
 var dma = require('..');
 
-var clasifier = dma(new Date('2015-05-14'), 10);
+var clasifier = dma(new Date('2015-05-14T00:00-0300'), 10);
 
 test('initial date is allways class 0', function(t) {
   t.plan(3);
-  t.equal(clasifier(new Date('2015-05-14')), 0);
-  t.equal(clasifier(new Date('2015-05-14T23:50')), 0);
-  t.equal(clasifier(new Date('2015-05-14T00:10')), 0);
+  t.equal(clasifier(new Date('2015-05-14T00:00-0300')), 0);
+  t.equal(clasifier(new Date('2015-05-14T23:50-0300')), 0);
+  t.equal(clasifier(new Date('2015-05-14T00:10-0300')), 0);
 });
 
 test('next day is class 1', function(t) {
   t.plan(1);
-  t.equal(clasifier(new Date('2015-05-15')), 1);
+  t.equal(clasifier(new Date('2015-05-15T00:00-0300')), 1);
 });
 
 test('previous day is class 9', function(t) {
   t.plan(1);
-  t.equal(clasifier(new Date('2015-05-13')), 9);
+  t.equal(clasifier(new Date('2015-05-13T20:00-0300')), 9);
 });
